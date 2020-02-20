bf460756632acda1060b12c298bfa23ea7acbfdf
Patil.vijay <patil.vijay@BANCTECINDIA.IN>
2020-02-20 14:59:21 +0530

    first commit

---
+++ src/main/java/com/financialconsulting/DevOpsDemoApp.java
@@ -0,0 +1,9 @@
+package com.financialconsulting;
+
+public class DevOpsDemoApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Dev Ops Demo Application !!!");
+	}
+
+}
